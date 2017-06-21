5a61e71cbab1c38c1f2ea389a903bc20ff9bec0c
Vishant <vishantjaiswal2k@gmail.com>
2017-06-21 17:28:36 +0530

    initial commit basic line graph

---
+++ app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityMain.java
@@ -0,0 +1,41 @@
+package com.wemen.samvedna.graphviewsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+        List<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry(1,5));
+        entries.add(new Entry(2,1));
+        entries.add(new Entry(3,2));
+        entries.add(new Entry(4,6));
+        entries.add(new Entry(5,8));
+        entries.add(new Entry(6,15));
+        entries.add(new Entry(7,35));
+        entries.add(new Entry(8,500));
+        entries.add(new Entry(9,-5));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(getResources().getColor(R.color.colorPrimary));
+        dataSet.setValueTextColor(getResources().getColor(R.color.colorAccent));
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+
+    }
+}
