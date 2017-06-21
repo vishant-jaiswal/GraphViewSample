cc9f513a2bbb85e866c58a92fb04f96b6711979f
Vishant <vishantjaiswal2k@gmail.com>
2017-06-21 17:55:38 +0530

    Implementation of onValueSelected Listener

--- app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityMain.java
+++ app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityMain.java
@@ -1,17 +1,21 @@
 package com.wemen.samvedna.graphviewsamples;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements OnChartValueSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +32,30 @@ public class ActivityMain extends AppCompatActivity {
         entries.add(new Entry(6,15));
         entries.add(new Entry(7,35));
         entries.add(new Entry(8,500));
-        entries.add(new Entry(9,-5));
+        entries.add(new Entry(9,-250));
 
         LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
         dataSet.setColor(getResources().getColor(R.color.colorPrimary));
         dataSet.setValueTextColor(getResources().getColor(R.color.colorAccent));
+
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.invalidate();
+        chart.setOnChartValueSelectedListener(this);
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        if(e.getY()>15){
+            Toast.makeText(this, "Value Greater than 100", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "less value ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected() {
 
     }
 }
