63c07404c18a84f0c02d29659b27718f5f3f6569
Vishant <vishantjaiswal2k@gmail.com>
2017-06-27 19:58:47 +0530

    Setting Data in LineChart

--- app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityLineChart.java
+++ app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityLineChart.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.EntryXComparator;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ActivityLineChart extends AppCompatActivity implements OnChartValueSelectedListener {
@@ -25,30 +32,63 @@ public class ActivityLineChart extends AppCompatActivity implements OnChartValue
         setContentView(R.layout.activity_line_chart);
 
         LineChart chart = (LineChart) findViewById(R.id.chart);
-        List<Entry> entries = new ArrayList<Entry>();
-        entries.add(new Entry(-1,50));
-        entries.add(new Entry(1,5));
-        entries.add(new Entry(2,1));
-        entries.add(new Entry(3,2));
-        entries.add(new Entry(4,6));
-        entries.add(new Entry(5,8));
-        entries.add(new Entry(6,15));
-        entries.add(new Entry(7,35));
-        entries.add(new Entry(8,500));
-        entries.add(new Entry(9,-250));
+        List<Entry> entriesC1 = new ArrayList<Entry>();
+        List<Entry> entriesC2 = new ArrayList<Entry>();
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-        dataSet.setColor(getResources().getColor(R.color.colorPrimary));
-        dataSet.setValueTextColor(getResources().getColor(R.color.colorAccent));
+        entriesC1.add(new Entry(0,1000));
+        entriesC1.add(new Entry(2,1100));
+        entriesC1.add(new Entry(6,1100));
+        entriesC1.add(new Entry(4,1300));
+        entriesC1.add(new Entry(5,1000));
+        entriesC1.add(new Entry(8,900));
+        Collections.sort(entriesC1,new EntryXComparator());
 
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
+
+        entriesC2.add(new Entry(0,1000));
+        entriesC2.add(new Entry(2,1200));
+        entriesC2.add(new Entry(4,1200));
+        entriesC2.add(new Entry(5,1000));
+        entriesC2.add(new Entry(7,1300));
+        entriesC2.add(new Entry(8,1200));
+
+        LineDataSet dataSetC1 = new LineDataSet(entriesC1, "Comp1"); // add entries to dataset
+        LineDataSet dataSetC2 = new LineDataSet(entriesC2,"Comp2");
+        /*dataSet.setColor(getResources().getColor(R.color.colorPrimary));
+        dataSet.setValueTextColor(getResources().getColor(R.color.colorAccent));*/
+
+        dataSetC1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        dataSetC1.setColor(Color.GREEN);
+        dataSetC2.setAxisDependency(YAxis.AxisDependency.LEFT);
+        dataSetC2.setColor(Color.RED);
+
+        List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(dataSetC1);
+        dataSets.add(dataSetC2);
+
+        final String[] quarters = new String[]{ "Q1", "Q2", "Q3", "Q4","Q4","Q5","Q6","Q7","Q8","Q9"};
+
+        IAxisValueFormatter formatter = new IAxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return quarters[(int) value];
+            }
+        };
+
         XAxis xAxis = chart.getXAxis();
-        xAxis.setLabelRotationAngle(90);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+
         YAxis yAxis = chart.getAxisLeft();
-        yAxis.setZeroLineWidth(50);
-        yAxis.setZeroLineColor(Color.GREEN);
-        xAxis.setDrawLabels(true);
+        yAxis.setDrawGridLines(false);
+
+        chart.getAxisRight().setDrawGridLines(false);
+
+
+
+        LineData lineData = new LineData(dataSets);
+        chart.setData(lineData);
         chart.invalidate();
         chart.setOnChartValueSelectedListener(this);
 
