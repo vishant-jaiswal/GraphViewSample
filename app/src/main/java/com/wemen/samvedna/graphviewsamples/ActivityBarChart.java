2bdb7c08de90a2d1007e064cf6b53cabbca0267b
Vishant <vishantjaiswal2k@gmail.com>
2017-06-27 20:47:13 +0530

    Setting Data in LineChart

--- app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityBarChart.java
+++ app/src/main/java/com/wemen/samvedna/graphviewsamples/ActivityBarChart.java
@@ -7,8 +7,10 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.EntryXComparator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ActivityBarChart extends AppCompatActivity {
     BarChart chart;
@@ -20,26 +22,26 @@ public class ActivityBarChart extends AppCompatActivity {
         chart = (BarChart) findViewById(R.id.bar_chart);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(4f, 1f));
         entries.add(new BarEntry(8f, 1));
         entries.add(new BarEntry(6f, 2));
         entries.add(new BarEntry(12f, 3));
         entries.add(new BarEntry(18f, 4));
         entries.add(new BarEntry(9f, 5));
+        //Collections.sort(entries,new EntryXComparator());
 
 
         BarDataSet dataset = new BarDataSet(entries, "# of Calls");
 
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
-
         BarData data = new BarData(dataset);
+        data.setBarWidth(0.8f);
         chart.setData(data);
-//        chart.saveToGallery("mychart.jpg", 85);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.setHighlightFullBarEnabled(false);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setFitBars(true);
+        chart.invalidate();
     }
 }
